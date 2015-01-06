f4f2d8b10acdf9e4627028bb46cc6f40d9c25c2b
thoggy <theoswe@gmail.com>
2015-01-07 00:33:04 +0100

    first commit

---
+++ node.js
@@ -0,0 +1,45 @@
+var KEYS = { 
+	UP : "1b5b41",
+	DOWN : "1b5b42", 
+	RIGHT : "1b5b43", 
+	LEFT : "1b5b44",
+	ESC : "1b"
+}
+
+process.stdin.setEncoding('hex');
+process.stdin.setRawMode(true);
+
+process.stdin.on("data", function(b){
+	
+	var keyAsHex = b.toString();
+
+	switch ( keyAsHex ) {
+		case KEYS.UP:
+			console.log("up");
+			break;
+		case KEYS.DOWN:
+			console.log("down");
+			break;
+		case KEYS.RIGHT:
+			console.log("right");
+			break;
+		case KEYS.LEFT:
+			console.log("left");
+			break;
+		case KEYS.ESC:
+			console.log("escape");
+			process.exit(0);
+			break;
+	}
+
+	// console.log( keyAsHex );
+});
+
+process.stdin.on("keypress", function(b){
+	console.log("hej");
+})
+
+process.on('exit', function(code) {
+	console.log('About to exit with code:', code);
+});
+
