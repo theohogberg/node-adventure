2c395a19d2b3b45fdca602366103c0abc19febdf
thoggy <theoswe@gmail.com>
2015-05-22 00:44:45 +0200

    updated some functions

--- node.js
+++ node.js
@@ -1,7 +1,6 @@
-var u = require('./utils');
-u.readJSON("loc01.json");
-
-process.stdout.write('\033c');
+var util = require('./utils');
+util.readJSON("loc01.json");
+var window_size;
 
 var KEYS = { 
 	UP : "1b5b41",
@@ -12,67 +11,81 @@ var KEYS = {
 };
 
 var UNICODE = {
-	LEFT : 0x219E,
-	UP : 0x219F,
-	RIGHT : 0x21A0,
-	DOWN : 0x21A1,
+	LEFT : 0x21e6,
+	UP : 0x21e7,
+	RIGHT : 0x21e8,
+	DOWN : 0x21e9,
 	RETURN : 0x23CE
 };
 
-process.stdin.setEncoding('hex');
-process.stdin.setRawMode(true);
+function initConsole (encoding, raw, clean, debug) {
+	
+	if (clean)
+		process.stdout.write('\033c');
+	window_size = process.stdout.getWindowSize();
+	process.stdin.setEncoding(encoding);
+	process.stdin.setRawMode(raw);
+	process.stdin.on("data", function(b){
 
-process.stdin.on("data", function(b){
+		process.stdout.write('\033c'); //clear
 
-	// Clear console
-	process.stdout.write('\033c');
+		var keyAsHex = b.toString();
+		switch ( keyAsHex ) {
+			case KEYS.UP:
+				process.stdout.write(String.fromCharCode(UNICODE.UP)+'\n');
+				break;
+			case KEYS.DOWN:
+				process.stdout.write(String.fromCharCode(UNICODE.DOWN)+'\n');
+				break;
+			case KEYS.RIGHT:
+				process.stdout.write(String.fromCharCode(UNICODE.LEFT)+'\n');
+				break;
+			case KEYS.LEFT:
+				process.stdout.write(String.fromCharCode(UNICODE.RIGHT)+'\n');
+				break;
+			case KEYS.ESC:
+				process.stdout.write(String.fromCharCode(UNICODE.RETURN)+'\n');
+				process.exit(0);
+				break;
+		}
 
-	var keyAsHex = b.toString();
-	switch ( keyAsHex ) {
-		case KEYS.UP:
-			process.stdout.write(String.fromCharCode(UNICODE.UP));
-			break;
-		case KEYS.DOWN:
-			process.stdout.write(String.fromCharCode(UNICODE.DOWN));
-			break;
-		case KEYS.RIGHT:
-			process.stdout.write(String.fromCharCode(UNICODE.LEFT));
-			break;
-		case KEYS.LEFT:
-			process.stdout.write(String.fromCharCode(UNICODE.RIGHT));
-			break;
-		case KEYS.ESC:
-			process.stdout.write(String.fromCharCode(UNICODE.RETURN));
-			process.exit(0);
-			break;
+	});
+
+	if (debug) {
+		if (process.stdout.isTTY) {
+			console.log("The console size is:", process.stdout.getWindowSize());
+		} else {
+			console.log("stdout is not a console");
+		}
+
+		process.on('exit', function(code) {
+			console.log('\nAbout to exit with code:', code);
+
+			if ( code === 0 ) {
+				console.log('exit success');	
+			} else {
+				console.log('exit failure');	
+			}
+
+		});
+
 	}
+	
+}
 
-	// console.log( keyAsHex );
-});
-
-
-
-process.on('exit', function(code) {
-	console.log('\nAbout to exit with code:', code);
-
-	if ( code === 0 ) {
-		console.log('exit success');	
-	} else {
-		console.log('exit failure');	
+function printAt (idx, idy, string) {
+	var cols = window_size[0];
+	// console.log(idy*cols);
+	for ( var i = 0 ; i < (idy*cols)+idx ; i++ ) {
+		process.stdout.write('1');
 	}
+	process.stdout.write(string);
+}
 
-});
 
-// function start (startfile) {
-// 	var zlib = require('zlib');
-// 	var gzip = zlib.createUnzip();
-// 	var fs = require('fs');
-// 	var inp = fs.createReadStream(startfile);
-// 	inp.pipe(gzip).pipe(process.stdout);
-// }
+initConsole('hex', true, true);
 
-if (process.stdout.isTTY) {
-	console.log("The console size is:", process.stdout.getWindowSize());
-} else {
-	console.log("stdout is not a console");
-} 
+printAt(1, 0, '0');
+
+
+
